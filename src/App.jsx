e642878f98e8835c7ed97c6162b079e3812c8945
Charlene Lugtu <clugtu.student@ua.edu.ph>
2025-10-23 14:25:54 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import "./App.css";
 
-// Display Component Function
 function Display({ value }) {
   return (
     <input
@@ -13,7 +12,6 @@ function Display({ value }) {
   );
 }
 
-// Button Component Function
 function Button({ label, className = "", onClick }) {
   return (
     <button className={className} onClick={onClick}>
@@ -23,30 +21,38 @@ function Button({ label, className = "", onClick }) {
 }
 
 export default function App() {
- 
-  return (
-    <div className="container">
-      <div className="calculator">
-        <h1 className="header">CHARLENE LUGTU - IT3A</h1>
-        <Display value="10 Things That Require Zero Talent" />
-        <div className="buttons">
-          <Button label="7 - Being Coachable" />
-          <Button label="8 - Doing A Little Extra" />
-          <Button label="9 - Being Prepared" />
+  const [displayValue, setDisplayValue] = useState("10 Things That Require Zero Talent");
 
-          <Button label="4 - Having A Positive Attitude" />
-          <Button label="5 - Being Passionate" />
-          <Button label="6 - Using Good Body Language" />
+  const handleClick = (value) => {
+    setDisplayValue(value);
+  };
 
-          <Button label="1 - Being on Time" />
-          <Button label="2 - Making An Effort" />
-          <Button label="3 - Being High Energy" />
+return (
+  <div className="container">
+    <div className="calculator">
+      <h1 className="header">CHARLENE LUGTU- IT3A</h1>
 
-          <Button label="CHARLENE LUGTU" />
-          <Button label="0 - Having A Strong Work Ethic" />
-          <Button label="C-PCIT9" />
-        </div>
+      <Display value={displayValue} />
+
+      <div className="buttons">
+
+        <Button label="7" onClick={() => handleClick("Being Coachable")} />
+        <Button label="8" onClick={() => handleClick("Doing A Little Extra")} />
+        <Button label="9" onClick={() => handleClick("Being Prepared")} />
+
+        <Button label="4" onClick={() => handleClick("Having A Positive Attitude")} />
+        <Button label="5" onClick={() => handleClick("Being Passionate")} />
+        <Button label="6" onClick={() => handleClick("Using Good Body Language")} />
+
+        <Button label="1" onClick={() => handleClick("Being On Time")} />
+        <Button label="2" onClick={() => handleClick("Making An Effort")} />
+        <Button label="3" onClick={() => handleClick("Being High Energy")} />
+
+        <Button label="NAME" onClick={() => handleClick("CHARLENE LUGTU")} />
+        <Button label="0" onClick={() => handleClick("Having A Strong Work Ethic")} />
+        <Button label="SUBJ" onClick={() => handleClick("C-PCIT9")} />
       </div>
     </div>
-  );
+  </div>
+);
 }
